43cc98470180d88738344fb1d17d1bc6cae9df94
austin207 <austinantony06@gmail.com>
2025-04-12 09:41:27 +0530

    second commit

--- src/pages/founders/FounderAzeem.tsx
+++ src/pages/founders/FounderAzeem.tsx
@@ -15,7 +15,7 @@ const FounderAzeem = () => {
         <div className="flex flex-col md:flex-row gap-12 items-start">
           <div className="w-full md:w-1/3">
             <img 
-              src="/Azeem1.jpg" 
+              src="/Azeem.jpg" 
               alt="A. Azeem Kouther" 
               className="rounded-xl shadow-lg w-full object-cover"
             />
